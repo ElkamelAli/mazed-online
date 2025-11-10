865d9e8bf3476603533d584d93f4f94dd451b8e6
Ali El kamel <bailhem1@gmail.com>
2025-11-10 22:00:11 +0100

    commit 10-11

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const User = mongoose.model('User', new mongoose.Schema({
     fullname: String,
     email: { type: String, unique: true },
     password: String,
+    phone: String
 }));
 
 const bcrypt = require('bcryptjs');
@@ -26,10 +27,10 @@ const jwt = require('jsonwebtoken');
 // Register
 app.post('/register', async (req, res) => {
   console.log("trying to register.....");
-    const { fullname, email, password } = req.body;
+    const { fullname, email, password, phone } = req.body;
     const hashed = await bcrypt.hash(password, 10);
     try {
-        const user = await User.create({ fullname, email, password: hashed });
+        const user = await User.create({ fullname, email, password: hashed, phone });
         res.json({ success: true });
     } catch (err) {
         res.status(400).json({ error: "Email already exists" });
